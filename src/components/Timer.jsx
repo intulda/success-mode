2060f7bff8192b8a6e2efca365c77fe6aaaf4a92
kimbogeun <intulda@naver.com>
2021-04-22 11:40:00 +0900

    타이머 정리

--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -3,20 +3,24 @@ import React, { useEffect, useRef, useState } from 'react';
 function Timer(closingDay) {
   const [today, setToday] = useState(new Date().getTime());
   const count = useRef(null);
+  const initialVariable = {
+    CLOSE: '경매 종료',
+  }
 
   useEffect(() => {
     const timer = setInterval(() => {
       setToday(new Date().getTime());
-      const distance = new Date(closingDay).getTime() - today;
-      const day = Math.floor(distance / (1000 * 60 * 60 * 24));
-      const hour = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-      const min = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-      const second = Math.floor((distance % (1000 * 60)) / 1000);
-      if(distance < 0) {
-        count.current = `경매 종료`;
+      const diff = new Date(closingDay).getTime() - today;
+      const day = Math.floor(diff / (1000 * 60 * 60 * 24));
+      const hour = Math.floor((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+      const min = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
+      const second = Math.floor((diff % (1000 * 60)) / 1000);
+
+      if(diff < 0) {
+        count.current = initialVariable.CLOSE;
       } else {
-        const result = `D-${day} ${hour.toString().length < 2 ? '0' + hour : hour}:${min.toString().length < 2 ? '0' + min : min}:${second.toString().length < 2 ? '0' + second : second} 남음`;
-        count.current = result;
+        const day = `D-${day} ${hour.toString().length < 2 ? '0' + hour : hour}:${min.toString().length < 2 ? '0' + min : min}:${second.toString().length < 2 ? '0' + second : second} 남음`;
+        count.current = day;
       }
     }, 1000);
 
